6b1acb85a63c506b69dfa1b4a19de779359a8dac
Lydiaprib <lydia.prado@alu.uclm.es>
2020-05-27 22:08:46 +0200

    Modificacion de los archivos y tratamiento de datos

--- clustering.py
+++ clustering.py
@@ -6,6 +6,7 @@ from sklearn.cluster import KMeans
 from mpl_toolkits.mplot3d import Axes3D
 import plotly
 import plotly.graph_objs as go
+import math
 
 def traza(x, y, z, c, label, s=2):
     puntosTraza = go.Scatter3d(
@@ -35,28 +36,29 @@ pd.set_option("display.max_rows", None)
 # Se carga el conjunto de datos que se van a usar para hacer clustering
 df_original = pd.read_csv("datos/Datos_Covid19_paises.csv", sep = ";", encoding='latin-1')
 # Se cambian los valores nulos por -1 para facilitar el tratamiento de los datos
-df_original = df_original.replace(np.nan, -1)
+df_original = df_original.replace(np.nan, 0)
 # Se genera la matriz de las variables que interesan para el estudio
-arrayVariables = df_original.iloc[:, [5, 8, 9]].values
+X = df_original.iloc[:, [5, 8, 9]].values
 # Se tratan los datos de la matriz para que estén en forma de integer
-for i in range(len(arrayVariables)):
-    population = [None] * len(arrayVariables)
-    for j in range(len(arrayVariables[i])):
-        if arrayVariables[i][j] != -1:
-            if ',' in arrayVariables[i][j]:
-                arrayVariables[i][j] = int(arrayVariables[i][j].replace(",", ""))
+for i in range(len(X)):
+    population = [None] * len(X)
+    for j in range(len(X[i])):
+        if X[i][j] != -1:
+            X[i][j] = float(str(X[i][j]).replace(",", ""))
         numeroHabitantes = df_original.iloc[i, [12]].values
         population[i] = numeroHabitantes[0].replace(",", "")      
         # Cálculo del número total de recuperados por cada millón de habitantes
-        if int(population[i]) != 0 and j == 0 and arrayVariables[i][j] != -1:
-            arrayVariables[i][j] = int(arrayVariables[i][j]) / int(population[i]) * 1000000
-    arrayVariables[i] = list(map(float, arrayVariables[i]))
+        if int(population[i]) != 0 and j == 0 and X[i][j] != -1:
+            X[i][j] = int(X[i][j]) / int(population[i]) * 1000000
+        X[i][j] = round(X[i][j] + 0.5)
+    X[i] = list(map(float, X[i]))
+print(X)
 
 
 wcss = []
 for i in range(1, 11):
-    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 100)
-    kmeans.fit(arrayVariables)
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 50)
+    kmeans.fit(X)
     wcss.append(kmeans.inertia_)
 
 # Grafica de la suma de las distancias
@@ -69,30 +71,33 @@ plt.show() # En esta gráfica se muestra que el número óptimo de clusters es 3
 
 
 kmeans = KMeans(n_clusters = 3, init = 'k-means++', random_state = 100)
-kmeans.fit(arrayVariables)
+kmeans.fit(X)
 centroides = kmeans.cluster_centers_
-y_kmeans = kmeans.fit_predict(arrayVariables)
-
+y_kmeans = kmeans.fit_predict(X)
+print(y_kmeans)
 # Se pinta cada uno de los clusters obtenidos en una gráfica 3D junto con sus centroides
-centroids = traza(centroides[:, 0], centroides[:, 1], centroides[:, 2], s= 8, c = 'yellow', label='Centroides')
-cluster1 = traza(arrayVariables[y_kmeans == 0, 0], arrayVariables[y_kmeans == 0, 1], arrayVariables[y_kmeans == 0, 2], s= 4, c='red', label = 'Cluster 1') #match with red=1 initial class
-cluster2 = traza(arrayVariables[y_kmeans == 1, 0], arrayVariables[y_kmeans == 1, 1], arrayVariables[y_kmeans == 1, 2], s= 4, c='black', label = 'Cluster 2') #match with black=3 initial class
-cluster3 = traza(arrayVariables[y_kmeans == 2, 0], arrayVariables[y_kmeans == 2, 1], arrayVariables[y_kmeans == 2, 2], s= 4, c='blue', label = 'Cluster 3') #match with blue=2 initial class
+centroids = traza(centroides[:, 0], centroides[:, 1], centroides[:, 2], s= 8, c = "silver", label = "Centroides")
+'''centroide1 = traza(centroides[0, 0], centroides[0, 1], centroides[0, 2], s= 8, c = "salmon", label = "Centroide1")
+centroide2 = traza(centroides[1, 0], centroides[1, 1], centroides[2, 2], s= 8, c = "pale green", label = "Centroide2")
+centroide3 = traza(centroides[2, 0], centroides[2, 1], centroides[2, 2], s= 8, c = "sky blue", label = "Centroide3")'''
+cluster1 = traza(X[y_kmeans == 0, 0], X[y_kmeans == 0, 1], X[y_kmeans == 0, 2], s= 4, c='red', label = 'Cluster 1') #match with red=1 initial class
+cluster2 = traza(X[y_kmeans == 1, 0], X[y_kmeans == 1, 1], X[y_kmeans == 1, 2], s= 4, c='green', label = 'Cluster 2') #match with black=3 initial class
+cluster3 = traza(X[y_kmeans == 2, 0], X[y_kmeans == 2, 1], X[y_kmeans == 2, 2], s= 4, c='blue', label = 'Cluster 3') #match with blue=2 initial class
 
 # Se establecen los ejes acordes a las variables de la matriz
-x=arrayVariables[:,0]
-y=arrayVariables[:,1]
-z=arrayVariables[:,2]
+x=X[:,0]
+y=X[:,1]
+z=X[:,2]
 
 # Se muestra la gráfica
-mostrarGrafica("K-Medias", "Número Recuperados", [min(x),max(x)], "Casos totales", [min(y),max(y)], "Muertes por millón de habitantes", [min(z)-1,max(z)], [t1,t2,t3,centroids])
+mostrarGrafica("K-Medias", "Número Recuperados", [min(x),max(x)], "Casos totales", [min(y),max(y)], "Muertes por millón de habitantes", [min(z)-1,max(z)], [cluster1, cluster2, cluster3, centroids])
 
 # Visualizacion grafica de los clusters
-plt.scatter(arrayVariables[:, 0], arrayVariables[:, 1], c=y_kmeans, s=50, cmap='viridis')
+plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=50, cmap='viridis')
 plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = 'pink', label = "Centroides")
 
-plt.title('Clusters of customers')
-plt.xlabel('Cases')
-plt.ylabel('Deaths')
+plt.title('Clustering Recuperados vs. Casos')
+plt.xlabel('Recuperados')
+plt.ylabel('Casos')
 plt.legend()
 plt.show()
